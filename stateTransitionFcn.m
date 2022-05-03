52b94e370ee6bd3c154ebbdf2875112924e0c50f
alandema <68517641+alandema@users.noreply.github.com>
2022-05-03 20:59:32 -0300

    Delete stateTransitionFcn.m

--- stateTransitionFcn.m
+++
@@ -1,8 +0,0 @@
-function predictParticles = stateTransitionFcn(pf, prevParticles,noise)
-
-predictParticles = prevParticles;
-predictParticles(:,1) = exp(-prevParticles(:,2)).*prevParticles(:,1);
-
-predictParticles = predictParticles + normrnd(0,noise,size(predictParticles));
-    
-end
